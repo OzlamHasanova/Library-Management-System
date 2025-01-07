bb363ba80e0805b0f06996703617fe39cc47f713
OzlamHasanova <7qk9bme@code.edu.az>
2025-01-08 02:07:31 +0400

    project created

---
+++ src/test/java/com/intelliacademy/orizonroute/librarymanagmentsystem/LibraryManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intelliacademy.orizonroute.librarymanagmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
